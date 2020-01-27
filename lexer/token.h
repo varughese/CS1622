96395f24cff3006ef37888c1824d7f5f5ed51574
Zhengming Wang <zmwang622@gmail.com>
2020-01-26 22:41:07 -0500

    Hola

--- lexer/token.h
+++ lexer/token.h
@@ -1,8 +1,13 @@
-typedef enum {
-	EOF,
-	ID,
-	NUM,
-	KEY,
-	SYM,
-	ERROR
-} token_t;
+#ifndef tokens
+#define tokens
+
+typedef enum token{
+	TOKEN_EOF,
+	TOKEN_ID,
+	TOKEN_NUM,
+	TOKEN_KEY,
+	TOKEN_SYM,
+	TOKEN_ERROR
+} token;
+
+#endif
