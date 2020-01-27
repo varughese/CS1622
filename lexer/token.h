52999220ef1e8040a98f125e67945be07d1f99e7
Mathew Varughese <mat.varughese3@gmail.com>
2020-01-27 16:13:02 -0500

    print out the tokens

--- lexer/token.h
+++ lexer/token.h
@@ -2,12 +2,19 @@
 #define tokens
 
 typedef enum token{
-	TOKEN_EOF,
+	TOKEN_EOF=0,
 	TOKEN_ID,
 	TOKEN_NUM,
 	TOKEN_KEY,
 	TOKEN_SYM,
 	TOKEN_ERROR
 } token;
+
+static inline char *tokenToString(enum token tok)
+{
+    static const char *strings[] = { "EOF", "ID", "NUM", "KEY", "SYM", "ERROR" };
+
+    return (unsigned char *) strings[tok];
+}
 
 #endif
