7c5557d1bc66165863df932f104b9acd023315f4
Mathew Varughese <mat.varughese3@gmail.com>
2020-01-29 17:28:29 -0500

    made test file better

--- lexer/test.sh
+++ lexer/test.sh
@@ -15,25 +15,37 @@ files=(
 # "test-invalid-mult-equal"
 # "test-invalid-plus-equal"
 # "test-keywords"
-"test-number-2"
-"test-number"
+# "test-number-2"
+# "test-number"
 # "test-program-gcd"
 # "test-program-selection-sort"
-# "test-program-simple"
-# "test-program-sum"
-# "test-stop-on-first-error-2"
-# "test-stop-on-first-error"
-# "test-symbols"
-# "test-whitespace-2"
+"test-program-simple"
+"test-program-sum"
+"test-stop-on-first-error-2"
+"test-stop-on-first-error"
+"test-symbols"
+"test-whitespace-2"
 # "test-whitespace"
 )
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+RESETCOLOR='\033[0m' # No Color
 
 /bin/bash ./compile.sh
 for f in "${files[@]}"
 do
 	/bin/bash ./run.sh test/cases/$f.in test/out_test/$f.out
+
+	diff_output="$(diff "test/cases/$f.out" "test/out_test/$f.out")"  
+	if [[ $diff_output == "" ]]; then
+    	echo -e "${GREEN}${f}${RESETCOLOR}"
+	else
+		echo -e "${RED}${f}${RESETCOLOR}"
+		echo "----"
+		cat test/cases/$f.in
+		echo "----"
+		sdiff test/cases/$f.out test/out_test/$f.out
+	fi
 	echo
-	echo
-	echo $f
-	diff test/cases/$f.out test/out_test/$f.out
 done
