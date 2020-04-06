dbcf2307e441d13ce23c3a4546c74b3d18ed9fd6
Mathew Varughese <mat.varughese3@gmail.com>
2020-04-06 11:10:03 -0400

    add output function

--- codegen/main.asm
+++ codegen/main.asm
@@ -1,6 +1,19 @@
+_f_output:
+sub $sp, $sp, 4
+sw $ra, 0($sp)
+lw $a0, 4($sp)li $v0, 1
+syscall
+li $v0, 11
+li $a0, 0x0a
+syscall
+
 .data
 a: .word 622
 
-__function :o
+_f_main:
 
-TODO - Turn ast into MIPS lol _L4
+# TODO - Turn ast into MIPS lol
+main:
+	jal _f_main
+	li $v0, 10 # We need to do this syscall to exit
+	syscall # Or else it will error!
--- codegen/mips_gen/mips_gen.c
+++ codegen/mips_gen/mips_gen.c
@@ -32,10 +32,6 @@ void decl_codegen(struct decl *d) {
 
 	struct symbol *sym = d->symbol;
 
-	if (d->type->kind == TYPE_FUNCTION) {
-		printf("__function :o\n");
-	}
-
 	if (d->type->kind == TYPE_INTEGER) {
 		if(sym->kind == SYMBOL_GLOBAL) {
 			const char *var_name = symbol_codegen(sym);
@@ -43,11 +39,25 @@ void decl_codegen(struct decl *d) {
 		}	
 	}
 
+	if (d->type->kind == TYPE_FUNCTION) {
+		printf("_f_%s:\n", d->symbol->name);
+	}
+
 	decl_codegen(d->next);
 }
 
 
+void include_output_input_functions() {
+	printf("_f_output:\nsub $sp, $sp, 4\nsw $ra, 0($sp)\nlw $a0, 4($sp)");
+	printf("li $v0, 1\nsyscall\nli $v0, 11\nli $a0, 0x0a\nsyscall\n\n");
+}
+
 void ast_to_mips(struct decl *root) {
+	include_output_input_functions();
 	decl_codegen(root);
-	printf("\nTODO - Turn ast into MIPS lol %s\n", label_name(4));
+	printf("\n# TODO - Turn ast into MIPS lol\n");
+	printf("main:\n");
+	printf("\tjal _f_main\n");
+	printf("\tli $v0, 10 # We need to do this syscall to exit\n");
+	printf("\tsyscall # Or else it will error!\n");
 }
